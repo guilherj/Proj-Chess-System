23cf243205c47b2e5b11bf44994b52ecbfb5edfb
Guilherme Nascimento <contato.guilhermenascimento@hotmail.com>
2021-01-25 10:08:34 -0300

    Starting to implement Board and Piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,4 +2,22 @@ package boardgame;
 
 public class Piece {
 
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+		/*
+		 * POR PADRÃO O JAVA JÁ COLOCA O ATRIBUTO POSITION COM VALOR NULL SEM NEM
+		 * PRECISAR ATRIBUIR NO CONSTRUTOR, SÓ DECLAREI PARA FICAR FACIL DE LEMBRAR
+		 */
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+
 }
